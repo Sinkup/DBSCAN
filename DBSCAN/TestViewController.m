9a28770810c7b154ecd91ebe1241f07395e6bb8a
Hao Chen <ruochenruofu@gmail.com>
2015-04-19 15:54:46 +0800

    add an effect picture

--- DBSCAN/TestViewController.m
+++ DBSCAN/TestViewController.m
@@ -22,48 +22,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-//        self.points = @[[NSValue valueWithCGPoint:CGPointMake(60, 40)],
-//                        [NSValue valueWithCGPoint:CGPointMake(20, 35)],
-//                        [NSValue valueWithCGPoint:CGPointMake(63, 67)],
-//                        [NSValue valueWithCGPoint:CGPointMake(25, 20)],
-//                        [NSValue valueWithCGPoint:CGPointMake(30, 23)],
-//                        [NSValue valueWithCGPoint:CGPointMake(23, 56)],
-//                        [NSValue valueWithCGPoint:CGPointMake(158, 455)],
-//                        [NSValue valueWithCGPoint:CGPointMake(34, 123)],
-//                        [NSValue valueWithCGPoint:CGPointMake(304, 212)],
-//                        [NSValue valueWithCGPoint:CGPointMake(131, 200)],
-//                        [NSValue valueWithCGPoint:CGPointMake(110, 240)],
-//                        [NSValue valueWithCGPoint:CGPointMake(140, 206)],
-//                        [NSValue valueWithCGPoint:CGPointMake(130, 280)],
-//                        [NSValue valueWithCGPoint:CGPointMake(212, 420)],
-//                        [NSValue valueWithCGPoint:CGPointMake(120, 320)],
-//                        [NSValue valueWithCGPoint:CGPointMake(20, 290)],
-//                        [NSValue valueWithCGPoint:CGPointMake(70, 250)],
-//                        [NSValue valueWithCGPoint:CGPointMake(250, 260)],
-//                        [NSValue valueWithCGPoint:CGPointMake(190, 450)],
-//                        [NSValue valueWithCGPoint:CGPointMake(170, 410)],
-//                        [NSValue valueWithCGPoint:CGPointMake(290, 40)],
-//                        [NSValue valueWithCGPoint:CGPointMake(40, 305)],
-//                        [NSValue valueWithCGPoint:CGPointMake(54, 434)],
-//                        [NSValue valueWithCGPoint:CGPointMake(38, 20)],
-//                        [NSValue valueWithCGPoint:CGPointMake(88, 23)],
-//                        [NSValue valueWithCGPoint:CGPointMake(23, 56)],
-//                        [NSValue valueWithCGPoint:CGPointMake(178, 455)],
-//                        [NSValue valueWithCGPoint:CGPointMake(34, 234)],
-//                        [NSValue valueWithCGPoint:CGPointMake(304, 520)],
-//                        [NSValue valueWithCGPoint:CGPointMake(131, 134)],
-//                        [NSValue valueWithCGPoint:CGPointMake(110, 222)],
-//                        [NSValue valueWithCGPoint:CGPointMake(140, 206)],
-//                        [NSValue valueWithCGPoint:CGPointMake(130, 280)],
-//                        [NSValue valueWithCGPoint:CGPointMake(112, 420)],
-//                        [NSValue valueWithCGPoint:CGPointMake(120, 320)],
-//                        [NSValue valueWithCGPoint:CGPointMake(80, 290)],
-//                        [NSValue valueWithCGPoint:CGPointMake(70, 250)],
-//                        [NSValue valueWithCGPoint:CGPointMake(250, 260)],
-//                        [NSValue valueWithCGPoint:CGPointMake(165, 450)],
-//                        [NSValue valueWithCGPoint:CGPointMake(200, 410)],
-//                        ];
     }
+    
     return self;
 }
 
@@ -77,8 +37,8 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-//    self.points = [NSArray arrayWithContentsOfFile:[path stringByAppendingPathComponent:@"points"]];
     self.points = [NSKeyedUnarchiver unarchiveObjectWithFile:[path stringByAppendingPathComponent:@"points"]];
+    
     if (!self.points || self.points.count <= 0) {
         CGSize size = [UIScreen mainScreen].bounds.size;
         int32_t width = size.width;
@@ -89,8 +49,7 @@
             CGPoint point = CGPointMake(arc4random() % width * 1.0, arc4random() % height * 1.0);
             [array addObject:[NSValue valueWithCGPoint:point]];
         }
-        
-//        BOOL success = [array writeToFile:[path stringByAppendingPathComponent:@"points"] atomically:YES];
+
         [NSKeyedArchiver archiveRootObject:array toFile:[path stringByAppendingPathComponent:@"points"]];
         self.points = array;
     }
@@ -117,7 +76,7 @@
 {
     [super viewDidAppear:animated];
     
-    DBSCAN *cluter = [[DBSCAN alloc] initWithRadius:100 minNumberOfPointsInCluster:2 andDistanceCalculator:^float(id obj1, id obj2) {
+    DBSCAN *cluter = [[DBSCAN alloc] initWithRadius:256 minNumberOfPointsInCluster:2 andDistanceCalculator:^float(id obj1, id obj2) {
         CGPoint point1 = [(NSValue *)obj1 CGPointValue];
         CGPoint point2 = [(NSValue *)obj2 CGPointValue];
         
